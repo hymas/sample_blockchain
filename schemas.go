e6baa70e62085b4531754ef12d65cbb5565f51be
hymas <hymas@de.ibm.com>
2017-04-23 00:50:19 +0200

    trim back to org

--- schemas.go
+++ schemas.go
@@ -50,97 +50,7 @@ var schemas = `
 							 "overallstatus": {
                                 "description": "overallstatus",
                                 "type": "string"
-                            },
-
-							// WJH
-							
-							/*
-																		
-							"gyro_x": 		{	
-								"description": "gyro_x",
-								"type": "string"
-							}
-							
-						
-							
-							"ambient_temp":	{	
-								"description": "ambient_temp",
-								"type": "string"
-							},
-							"gyro_y":		{	
-								"description": "gyro_y",
-								"type": "string"
-							},
-							"gyro_z":		{	
-								"description": "gyro_z",
-								"type": "string"
-							},
-							"compass_x":	{	
-								"description": "compass_x",
-								"type": "string"
-							},
-							"object_temp":	{	
-								"description": "object_temp",
-								"type": "string"
-							},
-							"compass_y":	{	
-								"description": "compass_y",
-								"type": "string"
-							},
-							"compass_z":	{	
-								"description": "compass_z",
-								"type": "string"
-							},
-							"acc_z":		{	
-								"description": "acc_z",
-								"type": "string"
-							},
-							"humidity":		{	
-								"description": "humidity",
-								"type": "string"
-							},
-							"acc_y":		{	
-								"description": "acc_y",
-								"type": "string"
-							},
-							"acc_x":		{	
-								"description": "acc_x",
-								"type": "string"
-							},
-							"air_pressure":	{	
-								"description": "air_pressure",
-								"type": "string"
-							},
-							"light":		{	
-								"description": "light",
-								"type": "string"
-							},
-							"deviceId":		{	
-								"description": "deviceId",
-								"type": "string"
-							},
-							"deviceType":	{	
-								"description": "deviceType",
-								"type": "string"
-							},
-							"eventType":	{	
-								"description": "eventType",
-								"type": "string"
-							},
-							"format":		{	
-								"description": "format",
-								"type": "string"
-							},
-							"_msgid":		{	
-								"description": "_msgid",
-								"type": "string"
-							}
-							*/                            
-                            
-                            // end WJH   
-							
-							                           
-                            
+                            }
                         },
                         "required": [
                             "assetID"
@@ -299,15 +209,6 @@ var schemas = `
                                 "description": "overallstatus",
                                 "type": "string"
                             }
-                            
-                            // WJH
-							/*											
-							"gyro_x": 		{	
-								"description": "gyro_x",
-								"type": "string"
-							}
-							*/
-                            
                     },
                     "type": "object"
                 }
@@ -411,16 +312,7 @@ var schemas = `
 							 "overallstatus": {
                                 "description": "overallstatus",
                                 "type": "string"
-                            },
-                            
-                            // WJH
-							/*											
-							"gyro_x": 		{	
-								"description": "gyro_x",
-								"type": "string"
-							}
-							*/
-                            
+                            }
                         },
                         "required": [
                             "assetID"
@@ -477,111 +369,26 @@ var schemas = `
                     },
                     "type": "object"
                 },
-				"role": {
-                    "description": "person role",
-                    "type": "string"
-                },
-				"lastowner": {
-                    "description": "lastowner name",
-                    "type": "string"
-                },
-				 "ownername": {
-                    "description": "ownername",
-                    "type": "string"
-                },
-				 "ownerid": {
-                    "description": "ownerid",
-                    "type": "string"
-                },
-				 "overallstatus": {
-                    "description": "overallstatus",
-                    "type": "string"
-                },
-                
-                // WJH
-				/*											
-				"gyro_x": 		{	
-					"description": "gyro_x",
-					"type": "string"
-				}
-				
-				
-				
-				"ambient_temp":	{	
-					"description": "ambient_temp",
-					"type": "string"
-				},
-				"gyro_y":		{	
-					"description": "gyro_y",
-					"type": "string"
-				},
-				"gyro_z":		{	
-					"description": "gyro_z",
-					"type": "string"
-				},
-				"compass_x":	{	
-					"description": "compass_x",
-					"type": "string"
-				},
-				"object_temp":	{	
-					"description": "object_temp",
-					"type": "string"
-				},
-				"compass_y":	{	
-					"description": "compass_y",
-					"type": "string"
-				},
-				"compass_z":	{	
-					"description": "compass_z",
-					"type": "string"
-				},
-				"acc_z":		{	
-					"description": "acc_z",
-					"type": "string"
-				},
-				"humidity":		{	
-					"description": "humidity",
-					"type": "string"
-				},
-				"acc_y":		{	
-					"description": "acc_y",
-					"type": "string"
-				},
-				"acc_x":		{	
-					"description": "acc_x",
-					"type": "string"
-				},
-				"air_pressure":	{	
-					"description": "air_pressure",
-					"type": "string"
-				},
-				"light":		{	
-					"description": "light",
-					"type": "string"
-				},
-				"deviceId":		{	
-					"description": "deviceId",
-					"type": "string"
-				},
-				"deviceType":	{	
-					"description": "deviceType",
-					"type": "string"
-				},
-				"eventType":	{	
-					"description": "eventType",
-					"type": "string"
-				},
-				"format":		{	
-					"description": "format",
-					"type": "string"
-				},
-				"_msgid":		{	
-					"description": "_msgid",
-					"type": "string"
-				}                            
-				// WJH end
-				*/
-				
+							 "role": {
+                                "description": "person role",
+                                "type": "string"
+                            },
+							"lastowner": {
+                                "description": "lastowner name",
+                                "type": "string"
+                            },
+							 "ownername": {
+                                "description": "ownername",
+                                "type": "string"
+                            },
+							 "ownerid": {
+                                "description": "ownerid",
+                                "type": "string"
+                            },
+							 "overallstatus": {
+                                "description": "overallstatus",
+                                "type": "string"
+                            }
             },
             "required": [
                 "assetID"
@@ -629,112 +436,26 @@ var schemas = `
                     },
                     "type": "object"
                 },
-				"role": {
-                    "description": "person role",
-                    "type": "string"
-                },
-				"lastowner": {
-                    "description": "lastowner name",
-                    "type": "string"
-                },
-				 "ownername": {
-                    "description": "ownername",
-                    "type": "string"
-                },
-				 "ownerid": {
-                    "description": "ownerid",
-                    "type": "string"
-                },
-				 "overallstatus": {
-                    "description": "overallstatus",
-                    "type": "string"
-                },
-
-				// WJH
-				
-				/*
-															
-				"gyro_x": 		{	
-					"description": "gyro_x",
-					"type": "string"
-				}
-				
-				
-				"ambient_temp":	{	
-					"description": "ambient_temp",
-					"type": "string"
-				},
-				"gyro_y":		{	
-					"description": "gyro_y",
-					"type": "string"
-				},
-				"gyro_z":		{	
-					"description": "gyro_z",
-					"type": "string"
-				},
-				"compass_x":	{	
-					"description": "compass_x",
-					"type": "string"
-				},
-				"object_temp":	{	
-					"description": "object_temp",
-					"type": "string"
-				},
-				"compass_y":	{	
-					"description": "compass_y",
-					"type": "string"
-				},
-				"compass_z":	{	
-					"description": "compass_z",
-					"type": "string"
-				},
-				"acc_z":		{	
-					"description": "acc_z",
-					"type": "string"
-				},
-				"humidity":		{	
-					"description": "humidity",
-					"type": "string"
-				},
-				"acc_y":		{	
-					"description": "acc_y",
-					"type": "string"
-				},
-				"acc_x":		{	
-					"description": "acc_x",
-					"type": "string"
-				},
-				"air_pressure":	{	
-					"description": "air_pressure",
-					"type": "string"
-				},
-				"light":		{	
-					"description": "light",
-					"type": "string"
-				},
-				"deviceId":		{	
-					"description": "deviceId",
-					"type": "string"
-				},
-				"deviceType":	{	
-					"description": "deviceType",
-					"type": "string"
-				},
-				"eventType":	{	
-					"description": "eventType",
-					"type": "string"
-				},
-				"format":		{	
-					"description": "format",
-					"type": "string"
-				},
-				"_msgid":		{	
-					"description": "_msgid",
-					"type": "string"
-				} 
-				*/                                                                   
-                  
-                            
+							 "role": {
+                                "description": "person role",
+                                "type": "string"
+                            },
+							"lastowner": {
+                                "description": "lastowner name",
+                                "type": "string"
+                            },
+							 "ownername": {
+                                "description": "ownername",
+                                "type": "string"
+                            },
+							 "ownerid": {
+                                "description": "ownerid",
+                                "type": "string"
+                            },
+							 "overallstatus": {
+                                "description": "overallstatus",
+                                "type": "string"
+                            }
             },
             "type": "object"
         }
